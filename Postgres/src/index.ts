92575c72c7bf5c64b8d0ca156593276939a20575
Shinzo27 <20bmiit031@gmail.com>
2024-06-29 00:22:05 +0530

    Practice

--- Postgres/src/index.ts
+++ Postgres/src/index.ts
@@ -1,21 +1,18 @@
 import { Client } from 'pg'
 
 const client = new Client({
-    connectionString: "postgresql://neondb_owner:PbhrA9HaDYM0@ep-damp-credit-a1t2w5da.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+    connectionString: "postgresql://postgres:root@localhost/postgres"
 })
 
 async function createUsersTable() {
     await client.connect()
     const result = await client.query(`
-        CREATE TABLE users (
-            id SERIAL PRIMARY KEY,
-            username VARCHAR(50) UNIQUE NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-        );
+        SELECT users.username, addresses.city, addresses.country, addresses.street, addresses.pincode
+        FROM users
+        INNER JOIN addresses ON users.id = addresses.user_id;
     `)
     console.log(result)
 }
 
 createUsersTable()
+
