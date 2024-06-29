25beb2a3dfbaec49ebdcc32f9ea264c02baf24a3
Shinzo27 <20bmiit031@gmail.com>
2024-06-29 22:52:13 +0530

    orm practice

---
+++ orm/src/index.ts
@@ -0,0 +1,66 @@
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function insertUser(username: string, password: string, firstName: string, lastName: string) {
+    const res = await prisma.user.create({
+        data: {
+            username,
+            password,
+            firstName,
+            lastName
+        }
+    })
+    console.log(res);
+}
+
+// insertUser('admin1','123456',"Pratham","Patel")
+
+interface UpdateParams {
+    firstName: string
+    lastName: string
+}
+
+async function updateUser(username:string, {
+    firstName,
+    lastName
+}: UpdateParams) {
+    const res = await prisma.user.update({
+        where: { username },
+        data: {
+            firstName,
+            lastName
+        }
+    })
+    console.log(res)
+}
+
+// updateUser("admin1", {
+//     firstName: "Shinzo",
+//     lastName: "27k"
+// })
+
+async function getUser(username:string) {
+    const user = await prisma.user.findFirst({
+        where: {
+            username: username
+        }
+    })
+    console.log(user)
+}
+
+// getUser('admin1')
+
+async function createTodo(userId:number, title: string, description: string, done: boolean) {
+    const res = await prisma.todos.create({
+        data: {
+            title,
+            description,
+            userId,
+            done
+        }
+    })
+    console.log(res);
+}
+
+createTodo(1,"Go to gym","Go to gym at 6pm",false)
