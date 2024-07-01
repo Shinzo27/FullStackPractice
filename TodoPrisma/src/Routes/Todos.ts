4ea1308e34bdd8cc382c844798c9fb2fb0e782ed
Shinzo27 <20bmiit031@gmail.com>
2024-07-01 15:32:14 +0530

    update

--- TodoPrisma/src/Routes/Todos.ts
+++ TodoPrisma/src/Routes/Todos.ts
@@ -1,9 +1,70 @@
 import { Router, Request, Response } from "express";
+import { PrismaClient } from '@prisma/client'
 
-const router:Router = Router()
+const prisma = new PrismaClient()
+const router = Router()
 
-router.get('/getTodos', ()=>{
+router.get('/getTodos', async(req:Request, res:Response)=>{
+    const todos = await prisma.todos.findMany({})
+    res.status(200).json({
+        success: true,
+        todos
+    })
+})
 
+router.post('/addTodo', async(req:Request, res:Response)=>{
+    console.log(req.body);
+    const { title, description, done, userId } = req.body
+    const response = await prisma.todos.create({
+        data: {
+            title,
+            description,
+            done,
+            userId
+        }
+    })
+
+    res.status(200).json({
+        success: true,
+        message: "Todo created!"
+    })
+})
+
+router.put('/markAsDone/:id', async(req,res)=>{
+    const id:number = parseInt(req.params.id)
+
+    const response = await prisma.todos.update({
+        where: {
+            id: id
+        },
+        data: {
+            done: "True"
+        }
+    })
+
+    if(response) {
+        res.status(200).json({
+            success: true,
+            message: "Todo Updated!"
+        })
+    }
+})
+
+router.delete('/deleteTodo/:id', async(req,res)=>{
+    const id:number = parseInt(req.params.id)
+
+    const response = await prisma.todos.delete({
+        where: {
+            id
+        }
+    })
+
+    if(response) {
+        res.status(200).json({
+            success: true,
+            message: "Todo deleted successfully!"
+        })
+    }
 })
 
 export default router
--- TodoPrisma/src/index.ts
+++ TodoPrisma/src/index.ts
@@ -2,12 +2,17 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient();
 import express, {Express, Request, Response} from 'express'
+import todoRouter from './Routes/Todos.js'
 
 const app:Express = express()
+
+app.use(express.json())
 
 app.get('/',(req: Request, res: Response)=>{
     res.send("Hello World!")
 })
+
+app.use('/todos', todoRouter)
 
 app.listen(8000, ()=>{
     console.log("Server Started at 8000");
