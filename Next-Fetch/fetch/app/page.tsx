dbc60113c040fc55fdcbae5d38fbdb71a45cffb4
Shinzo27 <20bmiit031@gmail.com>
2024-07-26 01:28:20 +0530

    next

--- Next-Fetch/fetch/app/page.tsx
+++ Next-Fetch/fetch/app/page.tsx
@@ -1,14 +1,16 @@
 import axios from "axios";
 
+
 async function getDetails() {
-  const response = await axios.get('https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details', {withCredentials: true})
+  await new Promise((r)=>setTimeout(r,5000))
+  const response = await axios.get('https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details')
   return response.data
 }
+
 export default async function Home() {
-
   const userData = await getDetails()
-
   return (
+    <>
     <div className="bg-gray-900 text-white min-h-screen flex justify-center items-center flex-col gap-5">
       <div>
         {userData.email}
@@ -17,5 +19,6 @@ export default async function Home() {
         {userData.name}
       </div>
     </div>
+    </>
   );
 }
