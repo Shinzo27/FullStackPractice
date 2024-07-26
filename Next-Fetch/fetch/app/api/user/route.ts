f842ad2e8a5ab67b0e551fc8271139ff4dff89c2
Shinzo27 <20bmiit031@gmail.com>
2024-07-26 14:20:29 +0530

    next

--- Next-Fetch/fetch/app/api/user/route.ts
+++ Next-Fetch/fetch/app/api/user/route.ts
@@ -1,6 +1,25 @@
+import { NextRequest, NextResponse } from "next/server"
+import { PrismaClient } from '@prisma/client'
+
+const client = new PrismaClient()
+
 export function GET() {
     return Response.json({
         email: "pratham@gmail.com",
         name: "pratham"
+    })
+}
+
+export async function POST(req: NextRequest) {
+    const body = await req.json()
+    await client.user.create({
+        data: {
+            username: body.username,
+            password: body.password
+        }
+    })
+
+    return Response.json({
+        message: "User logged in!"
     })
 }
---
+++ Next-Fetch/fetch/app/signup/page.tsx
@@ -0,0 +1,5 @@
+import { Signup } from '@/Components/Signup'
+
+export default function (){
+    return <Signup/>
+}
