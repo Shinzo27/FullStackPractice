4be5ae88b7ea7dd91512bbe3f61430a8fd4fba03
Shinzo27 <20bmiit031@gmail.com>
2024-08-30 23:20:48 +0530

    pubsub

---
+++ Singletone/src/pubsubManager.ts
@@ -0,0 +1,26 @@
+export class PubSubManager {
+    private static instance: PubSubManager;
+
+    private constructor(){
+
+    }
+
+    public static getInstace(): PubSubManager {
+        if(!PubSubManager.instance){
+            PubSubManager.instance = new PubSubManager()
+        }
+        return PubSubManager.instance;
+    }
+
+    addUserToStock(userId:string, stockTicker: string) {
+
+    }
+
+    removeUserFromStock(userId: string, stockTicker: string) {
+
+    }
+
+    forwardMessageToUser(userId: string, stockTicker: string, message: string){
+        
+    }
+}
