3a3f66d5a5b7ebc7784dc43c2c1c3b78f6ed9888
Shinzo27 <20bmiit031@gmail.com>
2024-10-01 21:47:37 +0530

    exchange

--- exchange-project/app/components/Depth/AskTable.tsx
+++ exchange-project/app/components/Depth/AskTable.tsx
@@ -1,12 +1,60 @@
 const AskTable = ({asks}: { asks: [string, string][] }) => {
     let currentTotal = 0;
     const releventAsks = asks.slice(0, 15)
+
+    releventAsks.reverse()
     
+    let askWithTotal: [string, string, number][] = []
+    for(let i= releventAsks.length-1; i>=0;i--){{
+        const [price, quantity] = releventAsks[i]
+        askWithTotal.push([price, quantity, currentTotal += Number(quantity)])
+    }
+    const maxTotal = releventAsks.reduce((acc, [_, quantity]) => acc + Number(quantity), 0)
+
+    askWithTotal.reverse()
+
     return (
         <div>
-            AskTable
+            {askWithTotal.map(([price, quantity, total]) => <Ask maxTotal={maxTotal} price={price} quantity={quantity} total={total} key={price}/>)}
         </div>
     );
 }
+}
+
+
+function Ask({price, quantity, total, maxTotal}: {price: string, quantity: string, total: number, maxTotal: number}) {
+    return <div
+    style={{
+        display: "flex",
+        position: "relative",
+        width: "100%",
+        backgroundColor: "transparent",
+        overflow: "hidden",
+    }}>
+    <div
+        style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: `${(100 * total) / maxTotal}%`,
+        height: "100%",
+        background: "rgba(228, 75, 68, 0.325)",
+        transition: "width 0.3s ease-in-out",
+        }}
+    ></div>
+    <div className="flex justify-between text-xs w-full">
+        <div>
+            {price}
+        </div>
+        <div>
+            {quantity}
+        </div>
+        <div>
+            {total?.toFixed(2)}
+        </div>
+    </div>
+    </div>
+}
+
 
 export default AskTable;
--- exchange-project/app/page.tsx
+++ exchange-project/app/page.tsx
@@ -6,25 +6,8 @@ import SwapUI from "./components/SwapUI";
 
 export default function Home() {
   return (
-    <div className="flex flex-row flex-1 justify-center items-center">
-      <div className="flex flex-col flex-1">
-        <MarketBar/>
-        <div className="flex flex-row h-[620px] border-y border-slate-800">
-          <div className="flex flex-col flex-1">
-            <TradeView/>
-          </div>
-          <div className="w-[1px] flex-col border-slate-800 border-l"></div>
-          <div className="flex flex-col w-[250px] overflow-hidden">
-            <Depth market="BTC_USDC"/>
-          </div>
-        </div>
-      </div>
-      <div className="w-[1px] flex-col border-slate-800 border-l"></div>
-      <div>
-        <div className="flex flex-col w-[250px]">
-          <SwapUI/>
-        </div>
-      </div>
+    <div>
+      Landing Page
     </div>
   );
 }
