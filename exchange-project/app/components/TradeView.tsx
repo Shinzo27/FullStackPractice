be69336c8b06754859fcab33307185c4446efdc4
Shinzo27 <20bmiit031@gmail.com>
2024-10-02 10:28:06 +0530

    charts

--- exchange-project/app/components/TradeView.tsx
+++ exchange-project/app/components/TradeView.tsx
@@ -1,7 +1,53 @@
-const TradeView = () => {
+import { useEffect, useRef, useState } from "react";
+import { KLine, Ticker } from "../utils/types";
+import { getKlines, getTicker } from "../utils/httpClient";
+import { ChartManager } from "../utils/ChartManager";
+
+const TradeView = ({ market }: { market: string }) => {
+    const chartRef = useRef<HTMLDivElement>(null);
+    const chartManagerRef = useRef<ChartManager>(null);
+
+    const init = async () => {
+        let klinesData: KLine[] = [];
+
+        try {
+            klinesData = await getKlines(market, "1h", Math.floor((new Date().getTime() - 1000 * 60 * 60 * 24 * 7) / 1000), Math.floor(new Date().getTime() / 1000));
+        } catch (error) {
+            console.log(error);
+        }
+
+        if(chartRef){
+            if(chartManagerRef.current){
+                chartManagerRef.current.destroy();
+            }
+            const chartManager = new ChartManager(
+                chartRef.current,
+                [
+                  ...klinesData?.map((x) => ({
+                    close: parseFloat(x.close),
+                    high: parseFloat(x.high),
+                    low: parseFloat(x.low),
+                    open: parseFloat(x.open),
+                    timestamp: new Date(x.end), 
+                  })),
+                ].sort((x, y) => (x.timestamp < y.timestamp ? -1 : 1)) || [],
+                {
+                  background: "#0e0f14",
+                  color: "white",
+                }
+            );
+            //@ts-ignore 
+            chartManagerRef.current = chartManager;
+        }
+    }
+
+    useEffect(() => {
+        init();
+    }, [market, chartRef]);
+
     return (
         <div>
-            Tradeview
+            <div ref={chartRef} style={{ height: "520px", width: "100%", marginTop: 4 }}></div>
         </div>
     );
 }
