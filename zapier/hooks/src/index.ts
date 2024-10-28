4c30fc58261614342a77e9cfd0ce5471d5f4d23e
Shinzo27 <20bmiit031@gmail.com>
2024-10-28 22:20:10 +0530

    v1

--- zapier/hooks/src/index.ts
+++ zapier/hooks/src/index.ts
@@ -1,13 +1,36 @@
-// import express from 'express';
+import express from 'express';
 
-// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-// const client = new PrismaClient();
+const client = new PrismaClient();
 
-// const app = express()
+const app = express()
 
-// app.use(express.json());
+app.use(express.json());
 
-// app.post('/hooks/catch/:userId/:zapId', async (req, res) => {
-    
-// })
+app.post('/hooks/catch/:userId/:zapId', async(req:any, res: any) => {
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+    const body = req.body;
+
+    await client.$transaction(async (tx) => {
+        const run = await tx.zapRun.create({
+            data: {
+                zapId,
+                metadata: body,
+            },
+        });
+
+        await tx.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id,
+            },
+        });
+    })
+
+    return res.json({ success: true })
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
