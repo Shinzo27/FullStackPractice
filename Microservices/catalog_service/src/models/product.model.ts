3b9052aaa5ce4754c788456315b1685f694c28e6
Shinzo27 <20bmiit031@gmail.com>
2024-12-15 21:25:34 +0530

    Changes

--- Microservices/catalog_service/src/models/product.model.ts
+++ Microservices/catalog_service/src/models/product.model.ts
@@ -1,28 +1,9 @@
-const mongoose = require('mongoose'); // Erase if already required
-
-// Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        unique:true,
-        index:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-});
-
-//Export the model
-module.exports = mongoose.model('User', userSchema);
+export class Product {
+    constructor(
+      public readonly name: string,
+      public readonly description: string,
+      public readonly price: number,
+      public readonly stock: number,
+      public readonly id?: number
+    ) {}
+  }
